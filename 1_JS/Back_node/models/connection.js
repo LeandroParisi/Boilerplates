e3d7517745b610f7e29a39c02befcf1704e64210
LeandroParisi <lepcbelisario@gmail.com>
2022-02-19 07:51:27 -0300

    FEAT: JS boilerplates

---
+++ 1_JS/Back_node/models/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2/promise');
+const connection = mysql.createPool({
+  user: 'root',
+  password: '',
+  host: 'localhost',
+  database: 'model_example',
+});
+
+module.exports = connection;
